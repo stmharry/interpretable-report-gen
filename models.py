6a613e8d3e47a9451d0bf47486dfaa9518d2f287
Tzu Ming Harry Hsu <stmharry@mit.edu>
2018-12-21 18:48:21 -0500

    add dropout and stuff

--- models.py
+++ models.py
@@ -1,77 +1,20 @@
-import re
 import torch
 import torch.nn.functional as F
-import torchvision
 
 from torch.nn import (
     Module,
     Embedding,
+    LSTMCell,
+    AdaptiveAvgPool2d,
     Conv2d,
     Linear,
-    ReLU,
     Dropout,
 )
 
-
-class ImageEncoder(Module):
-    image_embedding_size = 1024
-
-    def __init__(self, image_size=7):
-        super(ImageEncoder, self).__init__()
-
-        self.image_size = image_size
-
-        self.net = torchvision.models.DenseNet(num_init_features=64, growth_rate=32, block_config=(6, 12, 24, 16))
-
-    def load_state_dict(self, state_dict):
-        pattern = re.compile(r'^(.*denselayer\d+\.(?:norm|relu|conv))\.((?:[12])\.(?:weight|bias|running_mean|running_var))$')
-
-        _state_dict = {}
-        for key in state_dict.keys():
-            match = pattern.match(key)
-            if match:
-                _key = match.group(1) + match.group(2)
-            else:
-                _key = key
-
-            _state_dict[_key[19:]] = state_dict[key]
-
-        self.net.load_state_dict(_state_dict, strict=False)
-
-    def forward(self, v):
-        v = self.net.features(v)
-        v = F.adaptive_avg_pool2d(v, self.image_size)
-        v = v.view(-1, self.image_embedding_size, self.image_size * self.image_size).transpose_(1, 2)
-
-        return v
-
-
-class ReportDecoder(Module):
-    pass
-
-
-class LSTMCell(Module):
-    def __init__(self, input_size, hidden_size):
-        super(LSTMCell, self).__init__()
-
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-
-        self.fc = Linear(input_size + hidden_size, 5 * hidden_size)
-
-    def forward(self, x, _h, _m):
-        xh = torch.cat([x, _h], dim=1)
-        (i, f, o, g, m) = self.fc(xh).split([self.hidden_size] * 5, dim=1)
-
-        i = F.sigmoid(i)
-        f = F.sigmoid(f)
-        o = F.sigmoid(o)
-        m = i * F.tanh(m) + f * _m
-
-        h = o * F.tanh(m)
-        s = g * F.tanh(m)
-
-        return (h, m, s)
+from torchvision.models.resnet import (
+    ResNet,
+    Bottleneck,
+)
 
 
 class AdaptiveAttention(Module):
@@ -81,33 +24,145 @@ class AdaptiveAttention(Module):
         self.image_size = image_size
         self.hidden_size = hidden_size
 
+        self.fc = Linear(hidden_size, 2 * hidden_size)
         self.fc_v = Linear(hidden_size, hidden_size)
         self.fc_h = Linear(hidden_size, hidden_size)
         self.fc_s = Linear(hidden_size, hidden_size)
         self.fc_z = Linear(hidden_size, 1)
+        self.dropout = Dropout(0.5)
 
+    def forward(self, v, h):
+        """
 
-    def forward(self, _v, _h, _s):
-        '''
-        v: [batch_size, image_size * image_size, hidden_size]
-        h: [batch_size, hidden_size]
-        s: [batch_size, hidden_size]
-        '''
+        Args:
+            v (batch_size, image_size * image_size, hidden_size): Image feature map.
+            h (batch_size, hidden_size): Current hidden state.
 
-        _h = _h.unsqueeze_(1)
-        _s = _s.unsqueeze_(1)
+        Returns:
+            a (batch_size, image_size * image_size + 1): Attention weights.
+            c (batch_size, hidden_size): Context vector.
 
-        v = self.fc_v(_v)
-        h = self.fc_h(_h)
-        s = self.fc_s(_s)
+        """
 
-        z = F.tanh(torch.cat([v, h], 1) + s)
-        z = self.fc_z(z)
+        h = h.unsqueeze(1)
+        (h, s) = torch.tanh(self.fc(self.dropout(h))).split([self.hidden_size] * 2, 2)
+
+        _v = self.fc_v(self.dropout(v))
+        _h = self.fc_h(self.dropout(h))
+        _s = self.fc_s(self.dropout(s))
+
+        z = torch.tanh(torch.cat([_v, _s], 1) + _h)
+        z = self.fc_z(self.dropout(z)).squeeze(2)
         a = F.softmax(z, 1)
-
-        c = torch.sum(a * torch.cat([_v, _h], 1), 1)
+        c = torch.sum(a.unsqueeze(2) * torch.cat([v, s], 1), 1)
 
         return (a, c)
+
+
+class ImageEncoder(ResNet):
+    image_embedding_size = 2048
+
+    def __init__(self, image_size, embedding_size):
+        super(ImageEncoder, self).__init__(Bottleneck, [3, 4, 6, 3])
+
+        self.image_size = image_size
+        self.embedding_size = embedding_size
+
+        self.conv1 = Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.avgpool = AdaptiveAvgPool2d((image_size, image_size))
+        self.fc = Conv2d(self.image_embedding_size, embedding_size, (1, 1))
+        self.dropout = Dropout(0.5)
+
+    def forward(self, v):
+        """
+
+        Args:
+            v (batch_size, 1, 256, 256): Grayscale Image.
+
+        Returns:
+            v (batch_size, image_embedding_size, image_size, image_size): Image feature map.
+
+        """
+
+        v = self.conv1(v)
+        v = self.bn1(v)
+        v = self.relu(v)
+        v = self.maxpool(v)
+
+        v = self.layer1(v)
+        v = self.layer2(v)
+        v = self.layer3(v)
+        v = self.layer4(v)
+
+        v = self.avgpool(v)
+        v = self.dropout(v)
+        v = self.fc(v)
+        v = v.view(-1, self.embedding_size, self.image_size * self.image_size).transpose(1, 2)
+
+        return v
+
+
+class ReportDecoder(Module):
+    def __init__(self,
+                 label_size,
+                 embedding_size,
+                 hidden_size):
+
+        super(ReportDecoder, self).__init__()
+
+        self.label_size = label_size
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+
+        self.fc_h = Linear(embedding_size, hidden_size)
+        self.fc_m = Linear(embedding_size, hidden_size)
+        # (label, topic, temp, stop)
+        self.fc_sizes = [self.label_size, self.hidden_size, 1, 1]
+        self.fc = Linear(hidden_size, sum(self.fc_sizes))
+        self.lstm_cell = LSTMCell(embedding_size + label_size, hidden_size)
+        self.dropout = Dropout(0.5)
+
+    def forward(self, v, l, lengths):
+        """
+
+        Args:
+            v (batch_size, image_size * image_size, hidden_size): Image feature map.
+            l (batch_size, max_length, label_size): Interpretable labels.
+            lengths (batch_size,): Sequence lengths.
+
+        Returns:
+            label (batch_size, max_length, label_size): Generated labels.
+            topic (batch_size, max_length, embedding_size): Generated topic embeddings.
+            temp (batch_size, max_length, 1): Temperatures.
+            stop (batch_size, max_length, 1): Stop decoding.
+
+        """
+
+        (lengths, indices) = lengths.sort(0, descending=True)
+        v = v[indices]
+        l = l[indices]
+
+        vg = v.mean(1)
+
+        h = torch.tanh(self.fc_h(self.dropout(vg)))
+        m = torch.tanh(self.fc_m(self.dropout(vg)))
+
+        outputs = []
+        for t in range(max(lengths)):
+            batch_size_t = sum(lengths > t)
+
+            x = torch.cat([vg.expand(batch_size_t, -1), l[:batch_size_t, t]], 1)
+            (h, m) = self.lstm_cell(x, (h, m))
+            (label, topic, temp, stop) = F.relu(self.fc(self.dropout(h))).split(self.fc_sizes, 1)
+            # TODO: label, topic
+            temp = torch.exp(temp)
+            stop = torch.sigmoid(stop)
+
+            outputs.append((label, topic, temp, stop))
+
+        (label, topic, temp, stop) = [torch.nn.utils.rnn.pad_sequence(output) for output in zip(*outputs)]
+
+        return (label, topic, temp, stop)
 
 
 class SetenceDecoder(Module):
@@ -121,64 +176,91 @@ class SetenceDecoder(Module):
         super(SetenceDecoder, self).__init__()
 
         self.word_embedding = Embedding(vocab_size, embedding_size)
-        self.fc_v = Linear(image_embedding_size, embedding_size)
         self.fc_h = Linear(embedding_size, hidden_size)
         self.fc_m = Linear(embedding_size, hidden_size)
         self.lstm_cell = LSTMCell(2 * embedding_size, hidden_size)
         self.adaptive_attention = AdaptiveAttention(image_size, hidden_size)
         self.fc_p = Linear(hidden_size, vocab_size)
+        self.dropout = Dropout(0.5)
 
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
         self.hidden_size = hidden_size
 
     def forward(self, v, w, lengths):
-        '''
-        v: [batch_size, image_size * image_size, hidden_size]
-        w: [batch_size, max_length]
-        '''
+        """
+
+        Args:
+            v (batch_size, image_size * image_size, hidden_size): Image feature map.
+            w (batch_size, max_length): Sentences.
+            lengths (batch_size,): Sequence lengths.
+
+        Returns:
+            attention (batch_size, max_length, image_size * image_size + 1): Attention weights.
+            prob (batch_size, max_length, vocab_size): Generated probability on words.
+
+        """
+
+        max_length = max(lengths)
 
         (lengths, indices) = lengths.sort(0, descending=True)
         v = v[indices]
         w = w[indices]
 
         w = self.word_embedding(w)
-        v = self.fc_v(v)
         vg = v.mean(1)
 
-        _h = self.fc_h(vg)
-        _m = self.fc_m(vg)
-        for t in range(max(lengths)):
+        h = torch.tanh(self.fc_h(self.dropout(vg)))
+        m = torch.tanh(self.fc_m(self.dropout(vg)))
+
+        outputs = []
+        for t in range(max_length):
             batch_size_t = sum(lengths > t)
 
-            x = torch.cat([
-                w[:batch_size_t, t],
-                vg.expand(batch_size_t, -1),
-            ], 1)
-            (h, m, s) = self.lstm_cell(x, _h, _m)
-            (a, c) = self.adaptive_attention(v, h, s)
-            p = F.softmax(self.fc_p(c + h), 1)
+            x = torch.cat([vg.expand(batch_size_t, -1), w[:batch_size_t, t]], 1)
+            (h, m) = self.lstm_cell(x, (h, m))
+            (a, c) = self.adaptive_attention(v, h)
+            p = F.softmax(self.fc_p(self.dropout(c + h)), 1)
 
-            import pdb; pdb.set_trace()
+            outputs.append((a, p))
+
+        (attention, prob) = [torch.nn.utils.rnn.pad_sequence(output) for output in zip(*outputs)]
+
+        return (attention, prob)
 
 
 if __name__ == '__main__':
 
+    image_size = 8
+    label_size = 16
+    embedding_size = 256
+    hidden_size = 256
+    vocab_size = 1337
+
     device = torch.device('cuda')
+
     image_encoder = ImageEncoder(
-        image_size=7,
+        image_size=image_size,
+        embedding_size=embedding_size,
+    ).to(device)
+    report_decoder = ReportDecoder(
+        label_size=label_size,
+        embedding_size=embedding_size,
+        hidden_size=hidden_size,
     ).to(device)
     sentence_decoder = SetenceDecoder(
-        vocab_size=1337,
-        image_size=7,
+        vocab_size=vocab_size,
+        image_size=image_size,
         image_embedding_size=image_encoder.image_embedding_size,
-        embedding_size=256,
-        hidden_size=256,
+        embedding_size=embedding_size,
+        hidden_size=hidden_size,
     ).to(device)
 
-    v = torch.zeros((64, 3, 224, 224)).to(device)
-    w = torch.zeros((64, 32), dtype=torch.long).to(device)
-    lengths = torch.full((64,), 32, dtype=torch.long).to(device)
+    v = torch.zeros((16, 1, 256, 256)).to(device)
+    l = torch.zeros((16, 32, 16)).to(device)
+    w = torch.zeros((16, 32), dtype=torch.long).to(device)
+    lengths = torch.full((16,), 32, dtype=torch.long).to(device)
 
     v = image_encoder(v)
-    sentence_decoder(v, w, lengths)
+    (label, topic, temp, stop) = report_decoder(v, l, lengths)
+    # (a, p) = sentence_decoder(v, w, lengths)
